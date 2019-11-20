7104f55ac675213047ee21faa24735a3c8a1ed2c
Ben Strong <benjamin.strong@mymail.champlain.edu>
2019-11-20 13:41:31 -0500

    Commit

---
+++ Assets/Scripts/FuzzyLogicController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuzzyLogicController : MonoBehaviour
+{
+    public AnimationCurve critical, hurt, heatlthy;
+    public float health = 100.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        checkInput();
+
+    }
+
+    void checkInput()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            health -= 5f;
+        }
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            health += 5f;
+        }
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            health = 20f;
+        }
+    }
+
+    void evaluateValues() 
+    { 
+        
+    }
+}
